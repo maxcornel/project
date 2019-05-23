022c43d5721e49e77204ec4de0594c830d154103
MaxCornel <maxcornel@live.nl>
2019-05-23 15:40:22 +0200

    initial commit

--- scraper.py
+++ scraper.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
-driver.get('https://www.ah.nl/bonus')
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://www.jumbo.com/aanbiedingen')
+driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+
+images = driver.find_elements_by_tag_name('img')
+for image in images:
+    print(image.get_attribute('src'))
 
 driver.close()
