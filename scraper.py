14aef55f79b70c1ea2e538918cfbb98bb987b6ab
MaxCornel <maxcornel@live.nl>
2019-05-23 18:38:34 +0200

    tussenproduct

--- scraper.py
+++ scraper.py
@@ -5,21 +5,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import random
+import time
 
 outfile = open("bonus_lijst.html", "w")
 
 options = Options()
 options.headless = True
+
 driver = webdriver.Chrome(options=options)
 
 driver.get('https://www.ah.nl/bonus')
 
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(3)
 delay = 3 # seconds
 
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="epp_index_10"]/article[10]')))
-    print("Page is ready!")
+   myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="epp_index_10"]/article[10]')))
+   print("Page is ready!")
 except TimeoutException:
     print("Loading took too much time!")
 
@@ -27,9 +30,10 @@ products = driver.find_elements_by_class_name('product-cardview--bonus-group')
 
 outfile.write("<!doctype html>" +"\n" + "<html>" + "\n")
 
-
-for product in products:
-    nextline = str(product.get_attribute("outerHTML"))
+for i in range(3):
+    randomnumber = random.randint(0,len(products)-1)
+    product = products[randomnumber]
+    nextline = str(product.get_attribute("innerHTML"))
     outfile.write(nextline + "\n")
 
 outfile.write("</html>")
