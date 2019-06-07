d4c68aa1d0a25dc96f1010d0e4d064bd81866f7a
MaxCornel <maxcornel@live.nl>
2019-06-07 14:02:46 +0200

    dkfja

--- Program/Category randomizer.py
+++ Program/Category randomizer.py
@@ -7,16 +7,23 @@ def getbeginnum(IDnum):
         IDbegin[k] = v+1
     return IDbegin
 
-def numberincat():
+def numberincat(IDnum):
     numoftiles = len(Webscraper.getproducts())
+    listofplace = list(IDnum.values())
+    numincat = []
+    for i in range(len(listofplace)-1):
+        nextnum = listofplace[i+1] - listofplace[i]
+        numincat.append(nextnum)
+    lastnum = numoftiles - listofplace[len(listofplace)-1]
+    numincat.append(lastnum)
+    return numincat
 
-
-
-def getendnum(IDnum):
+'''def getendnum(IDnum):
     IDend = {}
     for (k,v)
-
-print(getbeginnum())
+'''
+IDnum = Webscraper.getid()
+print(numberincat(IDnum))
 
 
 
