ae7726f0a01534ebe43c9d4c8b2717de92291695
MaxCornel <maxcornel@live.nl>
2019-06-12 17:44:09 +0200

    Randomizer working

--- Program/Randomizer.py
+++ Program/Randomizer.py
@@ -10,29 +10,29 @@ def randommeal(meal):
         newlist = list(range(beginnumber[i],beginnumber[i]+numberincategory[i]))
         productranges[i] = newlist
 
-    randomproducten = []
+    randomproducts = []
     if meal == "ontbijt":
-        randomproducten.append(random.choice(productranges["zuivel-eieren"]))
-        randomproducten.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
-        randomproducten.append(random.choice(productranges["ontbijtgranen-broodbeleg-tussendoor"]))
+        randomproducts.append(random.choice(productranges["zuivel-eieren"]))
+        randomproducts.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
+        randomproducts.append(random.choice(productranges["ontbijtgranen-broodbeleg-tussendoor"]))
     elif meal == "lunch":
-        randomproducten.append(random.choice(productranges["kaas-vleeswaren-delicatessen"]))
-        randomproducten.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
-        randomproducten.append(random.choice(productranges["bakkerij"]))
+        randomproducts.append(random.choice(productranges["kaas-vleeswaren-delicatessen"]))
+        randomproducts.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
+        randomproducts.append(random.choice(productranges["bakkerij"]))
     elif meal == "avondeten":
-        randomproducten.append(random.choice(productranges["aardappel-groente-fruit"]))
-        randomproducten.append(random.choice(productranges["vlees-kip-vis-vega"]))
-        randomproducten.append(random.choice(productranges["soepen-conserven-sauzen-smaakmakers"]))
-        randomproducten.append(random.choice(productranges["pasta-rijst-internationale-keuken"]))
+        randomproducts.append(random.choice(productranges["aardappel-groente-fruit"]))
+        randomproducts.append(random.choice(productranges["vlees-kip-vis-vega"]))
+        randomproducts.append(random.choice(productranges["soepen-conserven-sauzen-smaakmakers"]))
+        randomproducts.append(random.choice(productranges["pasta-rijst-internationale-keuken"]))
     elif meal == "borrel":
-        randomproducten.append(random.choice(productranges["wijn"]))
-        randomproducten.append(random.choice(productranges["bier-sterke-drank-aperitieven"]))
-        randomproducten.append(random.choice(productranges["ontbijtgranen-broodbeleg-tussendoor"]))
+        randomproducts.append(random.choice(productranges["wijn"]))
+        randomproducts.append(random.choice(productranges["bier-sterke-drank-aperitieven"]))
+        randomproducts.append(random.choice(productranges["ontbijtgranen-broodbeleg-tussendoor"]))
     elif meal == "snellehap":
-        randomproducten.append(random.choice(productranges["verse-kant-en-klaar-maaltijden-salades"]))
-        randomproducten.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
+        randomproducts.append(random.choice(productranges["verse-kant-en-klaar-maaltijden-salades"]))
+        randomproducts.append(random.choice(productranges["frisdrank-sappen-koffie-thee"]))
     elif meal == "snoepen":
-        randomproducten.append(random.choice(productranges["snoep-koek-chips"]))
+        randomproducts.append(random.choice(productranges["snoep-koek-chips"]))
 
-    return randomproducten
+    return randomproducts
 
--- Program/outfilewriting.py
+++ Program/outfilewriting.py
@@ -1,1 +1,14 @@
-import Category randomizer
+import Randomizer
+import updatedata
+import readdata
+import datetime
+
+def getmymeal(meal):
+    lastupdate = datetime.date(2019,6,7)
+    if lastupdate == datetime.datetime.now():
+        updatedata.updatedate()
+
+    randomproducts = Randomizer.randommeal(meal)
+    products = readdata.loadproducts()
+
+    for i in randomproducts:
