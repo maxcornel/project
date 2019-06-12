9fd265272152c699eadae6bda896961d7f519c4b
MaxCornel <maxcornel@live.nl>
2019-06-12 19:56:56 +0200

    Almost done

--- Program/outfilewriting.py
+++ Program/outfilewriting.py
@@ -3,7 +3,10 @@ import updatedata
 import readdata
 import datetime
 
+
+
 def getmymeal(meal):
+    outputfile5 = open("sitepage", "w")
     lastupdate = datetime.date(2019,6,7)
     if lastupdate == datetime.datetime.now():
         updatedata.updatedate()
@@ -12,3 +15,11 @@ def getmymeal(meal):
     products = readdata.loadproducts()
 
     for i in randomproducts:
+        product = products[i]
+        nextline = str(product)
+        outputfile5.write(nextline + "\n")
+
+    outputfile5.write("</html>")
+    outputfile5.close()
+
+getmymeal("ontbijt")
